24a8b8bfaf5f4fbec98998d791903198a3b91c9d
C.J. Bordeleau <bordeleau.cj@gmail.com>
2012-11-23 15:13:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyapiFrontend::Application.config.secret_token = '4f7d342a9f4a3b0aa0d1b0419fd659934ff5e4efb89554e3fb2583bfa7a8c1c411819812d5b1369562494cb69891790e853dcb5e9469c1280970b059b6fa7b9e'
